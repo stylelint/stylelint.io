4bbcd510f426f5a6646924d8475e39d8c9c7752d
jeddy3 <jeddy3@users.noreply.github.com>
2016-02-14 11:58:22 +0000

    Do not generate feed.xml

--- scripts/webpack.config.babel.js
+++ scripts/webpack.config.babel.js
@@ -14,20 +14,6 @@ export default {
           `?${ JSON.stringify({
             context: path.join(config.cwd, config.source),
             basepath: config.baseUrl.path,
-            feedsOptions: {
-              title: pkg.name,
-              site_url: pkg.homepage,
-            },
-            feeds: {
-              "feed.xml": {
-                collectionOptions: {
-                  filter: { layout: "Post" },
-                  sort: "date",
-                  reverse: true,
-                  limit: 20,
-                },
-              },
-            },
           }) }`,
       },
       {
