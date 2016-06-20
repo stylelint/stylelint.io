a32bab45bbdcef6941940ca2d739d980b7934464
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2016-06-21 00:50:55 +0200

    (WIP) Added prototypical implementation

--- web_modules/layouts/Page/index.js
+++ web_modules/layouts/Page/index.js
@@ -6,15 +6,62 @@ import { BodyContainer } from "phenomic"
 
 import styles from "./index.css"
 
-function enhanceCodeBlocks(contents) {
-  return contents.replace(
-    /<p>The following patterns are considered warnings:<\/p>/,
-    "<p data-example-warning>The following patterns are considered warnings:<\/p>",
-  )
+function applyTransformsToContent(bodyContent) {
+  let invalidPatternFlag = false
+  let validPatternFlag = false
+
+  bodyContent.childNodes.forEach((node) => {
+    if (!node.classList) {
+      return
+    }
+
+    if (nodeIsValidPatternTrigger(node)) {
+      invalidPatternFlag = false
+      validPatternFlag = true
+      node.classList.add("valid-pattern")
+    }
+
+    if (nodeIsInvalidPatternTrigger(node)) {
+      invalidPatternFlag = true
+      validPatternFlag = false
+      node.classList.add("invalid-pattern")
+    }
+
+    if (nodeIsTriggerReset(node)) {
+      invalidPatternFlag = false
+      validPatternFlag = false
+    }
+
+    appendFlagsToPre(node, validPatternFlag, invalidPatternFlag)
+  })
+}
+
+function nodeIsValidPatternTrigger(node) {
+  const triggerText = "The following patterns are not considered warnings:"
+  return node.tagName === "P" && node.innerText === triggerText
+}
+
+function nodeIsInvalidPatternTrigger(node) {
+  const triggerText = "The following patterns are considered warnings:"
+  return node.tagName === "P" && node.innerText === triggerText
+}
+
+function nodeIsTriggerReset(node) {
+  const resetTriggers = [ "H1", "H2", "H3", "H4", "H5", "H6" ]
+
+  if (resetTriggers.indexOf(node.tagName) !== -1) {
+    return true
+  }
+}
+
+function appendFlagsToPre(node, validFlag, invalidFlag) {
+  if (node.tagName === "PRE") {
+    validFlag && node.classList.add("valid-pattern")
+    invalidFlag && node.classList.add("invalid-pattern")
+  }
 }
 
 export default class Page extends Component {
-
   static propTypes = {
     children: PropTypes.oneOfType([ PropTypes.array, PropTypes.object ]),
     __url: PropTypes.string.isRequired,
@@ -25,6 +72,10 @@ export default class Page extends Component {
   static contextTypes = {
     metadata: PropTypes.object.isRequired,
   };
+
+  componentDidMount() {
+    applyTransformsToContent(this._bodyContent)
+  }
 
   render() {
 
@@ -48,7 +99,12 @@ export default class Page extends Component {
           title={ title }
           meta={ meta }
         />
-        <BodyContainer>{ enhanceCodeBlocks(body) }</BodyContainer>
+        <BodyContainer>{
+          <div
+            ref={ (c) => { this._bodyContent = c } }
+            dangerouslySetInnerHTML={ { __html: body } }
+          />
+        }</BodyContainer>
         { this.props.children }
       </div>
     )
