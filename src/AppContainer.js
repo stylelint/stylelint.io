da727fc741af060fb6c7d49f59654af0d374c38f
Aleks Hudochenkov <aleks@hudochenkov.com>
2017-12-28 11:31:10 +0100

    Use prop-types library instead of React internal PropTypes (#50)

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from "react";
+import PropTypes from "prop-types";
+import React from "react";
 
 import Container from "./components/Container";
 import Content from "./components/Content";
