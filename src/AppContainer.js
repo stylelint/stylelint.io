34b95d05eadbf2092a6c1e989a7b0cb604edd938
Bilie <bilianavaleva@gmail.com>
2018-09-29 10:37:39 +0200

    Fix scroll to url fragment on initial load in Chrome

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import React from "react";
 
+import isChrome from "./utils/isChrome";
+
 import Container from "./components/Container";
 import Content from "./components/Content";
 import DefaultHeadMeta from "./components/DefaultHeadMeta";
@@ -13,18 +15,46 @@ import Navigation from "./components/Navigation";
 import "./index.global.css";
 import "./highlight.global.css";
 
-const AppContainer = props => (
-  <GoogleAnalyticsTracker params={props.params}>
-    <Container>
-      <DefaultHeadMeta />
-      <Header />
-      <Content>{props.children}</Content>
-      <Navigation />
-      <DocSearch />
-      <Footer />
-    </Container>
-  </GoogleAnalyticsTracker>
-);
+class AppContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    // Fix scroll to element from url fragment on initial load in Chrome
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=718468
+    if (isChrome() && window.location.hash) {
+      setTimeout(function() {
+        const elementId = window.location.hash.replace("#", "");
+        const scrollToElement = document.getElementById(elementId);
+
+        if (scrollToElement) {
+          scrollToElement.scrollIntoView();
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <GoogleAnalyticsTracker params={this.props.params}>
+        <Container>
+          <DefaultHeadMeta />
+          <Header />
+          <Content>{this.props.children}</Content>
+          <Navigation />
+          <DocSearch />
+          <Footer />
+        </Container>
+      </GoogleAnalyticsTracker>
+    );
+  }
+}
+
+AppContainer.propTypes = {
+  children: PropTypes.node,
+  params: PropTypes.object
+};
 
 AppContainer.propTypes = {
   children: PropTypes.node,
