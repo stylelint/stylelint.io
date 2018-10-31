341f1e38785b37460a771a99f2f42c018ed00ba7
Masafumi Koba <ybiquitous@gmail.com>
2018-10-31 21:40:04 +0900

    Fix title and description in demo page

--- src/layouts/DemoPage/index.js
+++ src/layouts/DemoPage/index.js
@@ -1,13 +1,22 @@
+import PropTypes from "prop-types";
 import React from "react";
+import Helmet from "react-helmet";
 
 import Demo from "../../components/Demo";
 
 import styles from "./index.css";
 
-const DemoPage = () => (
+const meta = head => [{ name: "description", content: head.description }];
+
+const DemoPage = ({ head }) => (
   <div className={styles.demoPage}>
+    <Helmet title={head.title} meta={meta(head)} />
     <Demo />
   </div>
 );
+
+DemoPage.propTypes = {
+  head: PropTypes.object.isRequired
+};
 
 export default DemoPage;
