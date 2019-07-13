16c97c608c45245d674d9700445287ca93efc4fe
Masafumi Koba <ybiquitous@gmail.com>
2019-07-14 00:32:12 +0900

    Delete Phenomic and update dependencies
    The migration to Docusaurus has worked (see #67), so this deletes Phenomic code as the old engine.
    
    - Delete unused directories and files.
    - Update dependencies.
    - Use Node 12 (latest) in Travis CI.

--- src/utils/isSafari.js
+++
@@ -1,8 +0,0 @@
-function isSafari() {
-  return (
-    window.navigator.userAgent.indexOf("Safari") !== -1 &&
-    window.navigator.vendor.indexOf("Apple Computer") !== -1
-  );
-}
-
-export default isSafari;
