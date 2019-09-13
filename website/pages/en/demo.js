968886b134e95726a5bcf41519e3b58fef8db4ff
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-09-13 18:10:56 +0200

    Update demo URL, when input/config changes. Restore demo state (#89)

--- website/pages/en/demo.js
+++ website/pages/en/demo.js
@@ -1,7 +1,29 @@
 const React = require("react");
 
+const FRAME_ORIGIN = "https://stylelint-demo.herokuapp.com";
+const inlineScript = `
+  var frame = document.getElementById("demo");
+
+  if (frame) {
+    window.addEventListener(
+      "message",
+      function(e) {
+        window.location.hash = e.data;
+      },
+      false
+    );
+    frame.src = "${FRAME_ORIGIN}" + window.location.hash;
+  }
+`;
+
 function Demo() {
-  return <iframe src="https://stylelint-demo.herokuapp.com" frameBorder="0" />;
+  return [
+    <iframe key="iframe-demo" id="demo" frameBorder="0" />,
+    <script
+      key="script-demo"
+      dangerouslySetInnerHTML={{ __html: inlineScript }}
+    />
+  ];
 }
 
 Demo.title = "Demo";
