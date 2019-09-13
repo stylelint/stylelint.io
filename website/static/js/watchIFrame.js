1bd42cc4715b1050fe8eb1b39f91588a6c3da5ce
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-09-13 08:53:29 +0300

    remove IE support in watchIFrame script

--- website/static/js/watchIFrame.js
+++ website/static/js/watchIFrame.js
@@ -1,19 +1,12 @@
-// Create IE + others compatible event handler
-var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-var eventer = window[eventMethod];
-var messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
-
-// Listen to message from child window
-eventer(
-  messageEvent,
-  function(e) {
-    window.location.hash = e.data;
-  },
-  false
-);
-
 var frame = document.getElementById("demo");
 
 if (frame) {
+  window.addEventListener(
+    "message",
+    function(e) {
+      window.location.hash = e.data;
+    },
+    false
+  );
   frame.src = frame.src + window.location.hash;
 }
