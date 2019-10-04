ab364dcf3618d74b9292b1ad14732bd181152efa
Aleks Hudochenkov <aleks@hudochenkov.com>
2019-10-04 22:32:23 +0200

    Remove extensive logging on build

--- scripts/generate-stylelint-docs.js
+++ scripts/generate-stylelint-docs.js
@@ -13,9 +13,11 @@ const siteConfig = require("../website/siteConfig");
 function mkdir_p(dir) {
   dir.split(path.sep).reduce((parentDir, currentDir) => {
     const newDir = path.join(parentDir, currentDir);
+
     if (!fs.existsSync(newDir)) {
       fs.mkdirSync(newDir);
     }
+
     return newDir;
   }, "");
 }
@@ -25,9 +27,11 @@ function processMarkdown(file, { rewriter }) {
     function visitor(node) {
       node.url = rewriter(node.url);
     }
+
     function transform(tree) {
       visit(tree, ["link"], visitor);
     }
+
     return transform;
   }
 
@@ -38,9 +42,11 @@ function processMarkdown(file, { rewriter }) {
 
   // Add Docusaurus-specific fields. See https://docusaurus.io/docs/en/doc-markdown
   let title = content.match(/\n?# ([^\n]+)\n/)[1];
+
   const titleToSidebarLabel = {
     stylelint: "Home"
   };
+
   const sidebarLabel = titleToSidebarLabel[title] || title;
 
   if (title === "stylelint") {
@@ -62,15 +68,15 @@ function generateSidebarsJson(outputDir, rulesDir) {
   const json = JSON.parse(
     fs.readFileSync(path.join(__dirname, "sidebars-template.json"), "utf8")
   );
+
   json.docs.Rules = fs
     .readdirSync(path.join(outputDir, rulesDir))
     .map(filename => `${rulesDir}/${path.basename(filename, ".md")}`)
     .sort();
 
   const outputFile = path.join("website", "sidebars.json");
-  fs.writeFileSync(outputFile, JSON.stringify(json, null, 2), "utf8");
 
-  return outputFile;
+  fs.writeFileSync(outputFile, JSON.stringify(json, null, 2), "utf8");
 }
 
 function addHostingInfo(content) {
@@ -103,9 +109,10 @@ function main(outputDir) {
         .replace("node_modules/stylelint", "")
         .replace("README.md", "index.md")
     );
+
     mkdir_p(path.dirname(outputFile));
+
     fs.writeFileSync(outputFile, output, "utf8");
-    console.log(outputFile);
   });
 
   glob.sync("node_modules/stylelint/docs/**/*.md").forEach(file => {
@@ -120,13 +127,15 @@ function main(outputDir) {
           .replace("../../lib/rules/", "rules/")
           .replace("/README.md", ".md")
     });
+
     const outputFile = path.join(
       outputDir,
       file.replace("node_modules/stylelint/docs", "")
     );
+
     mkdir_p(path.dirname(outputFile));
+
     fs.writeFileSync(outputFile, output, "utf8");
-    console.log(outputFile);
   });
 
   glob.sync("node_modules/stylelint/lib/rules/**/*.md").forEach(file => {
@@ -139,19 +148,22 @@ function main(outputDir) {
             "../$1.md"
           )
     });
+
     const outputFile = path.join(
       outputDir,
       file
         .replace("node_modules/stylelint/lib/rules", "user-guide/rules")
         .replace("/README.md", ".md")
     );
+
     mkdir_p(path.dirname(outputFile));
+
     fs.writeFileSync(outputFile, output, "utf8");
-    console.log(outputFile);
   });
 
-  const sidebarsJson = generateSidebarsJson(outputDir, "user-guide/rules");
-  console.log(`Generated: ${sidebarsJson}`);
+  generateSidebarsJson(outputDir, "user-guide/rules");
+
+  console.log("Documents have been generated.");
 }
 
 main(process.argv[2]);
